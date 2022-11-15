3cfad6a64606475ef3b4606c9c122fb6ee9e0a34
Anita <abatishev@csus.edu>
2022-11-15 14:56:25 -0800

    restOfLanding page

--- main/src/App.js
+++ main/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Privacy. Explanation about Omnistruct.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="test.html"
+          
           rel="noopener noreferrer"
         >
-          Learn React
+          Start
         </a>
       </header>
     </div>
