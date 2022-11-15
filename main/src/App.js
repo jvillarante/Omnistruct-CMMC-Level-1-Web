3858de9b1ff914b25975060697bc506087deaf50
dannyboy <danielperez2@csus.edu>
2022-11-15 15:28:26 -0800

    Updating app.js file to accept my html

--- main/src/App.js
+++ main/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="test.html"
+          href="questionPage.html"
           
           rel="noopener noreferrer"
         >
