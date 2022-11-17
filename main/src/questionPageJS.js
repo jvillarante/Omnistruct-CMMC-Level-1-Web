7389de009284095989316f9937566f6a64efbfce
dannyboy <danielperez2@csus.edu>
2022-11-16 21:57:29 -0800

    Created a new JavaScript File for the questionare

---
+++ main/src/questionPageJS.js
@@ -0,0 +1,30 @@
+import './App.css';
+//import './results.html';
+
+
+
+function Questionaire() {
+  return (
+  <div>
+    <div className="questionPage">
+      <header className="Question-header">
+        <p>
+          Omnistruct CMMC Level 1 Self Assessment
+        </p>
+        <a
+          className="Next-link"
+
+          href="resultsPage.html"
+          
+          rel="noopener noreferrer"
+        >
+          Next
+        </a>
+
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
