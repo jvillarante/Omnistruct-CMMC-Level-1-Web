733e3450509eb772895021526dba89aea9cf4ee7
Chase <crauhuff@csus.edu>
2022-11-19 10:59:50 -0800

    Added functions to display questions and to save answers

--- main/src/Assessment.js
+++ main/src/Assessment.js
@@ -1,15 +1,53 @@
 import React from 'react';
 
+var questions = ["Is a list of authorized users maintained that states their identity and role?", "Are requests to make system changes authorized?"];
+
+var questionAnswers = [[false, false], [false, false]];
+
+var questionNum = 0;
+
+function displayNextQuestion() {
+    questionAnswers[questionNum][0] = document.getElementById("yes").checked;
+    questionAnswers[questionNum][1] = document.getElementById("no").checked;
+    
+    if(questionNum !== questions.length - 1) questionNum++;
+
+    document.getElementById("yes").checked = questionAnswers[questionNum][0];
+    document.getElementById("no").checked = questionAnswers[questionNum][1];
+
+    if(questionNum > 0) document.getElementById("back").style.visibility = "visible";
+
+    document.getElementById("question").innerHTML = questions[questionNum];
+
+}
+
+function displayPreviousQuestion() {
+    questionAnswers[questionNum][0] = document.getElementById("yes").checked;
+    questionAnswers[questionNum][1] = document.getElementById("no").checked;
+    if(questionNum > 0) questionNum--;
+
+    document.getElementById("yes").checked = questionAnswers[questionNum][0];
+    document.getElementById("no").checked = questionAnswers[questionNum][1];
+    
+    if(questionNum === 0) document.getElementById("back").style.visibility = "hidden";
+
+    document.getElementById("question").innerHTML = questions[questionNum];
+}
+
 const Assessment = () => {
     return (
         <div className="assessment">
-            <p id = "question"></p>
-            <input id = "yes" type = "radio" name = "questionAnswer"/> Yes<br/>
-            <input id = "no"  type = "radio" name = "questionAnswer"/> No<br/>
-            <input id = "back" type = "button" value = "Back" onclick="displayPreviousQuestion();" style = {{ visibility: 'hidden' }}/>
-            <input type = "button" value = "Next" onclick="displayNextQuestion();"/> 
+            <p id = "question">Is a list of authorized users maintained that states their identity and role?</p>
+            <p>
+                <input id = "yes" type = "radio" name = "questionAnswer"/> Yes<br/>
+                <input id = "no"  type = "radio" name = "questionAnswer"/> No<br/>
+                <input id = "back" type = "button" value = "Back" onClick={displayPreviousQuestion} style = {{visibility: 'hidden' }}/>
+                <input type = "button" value = "Next" onClick={displayNextQuestion}/> 
+            </p>
         </div>
     )
 }
+
+
 
 export default Assessment;
