9b06eee5c4b8b2fff7e0d519b837c164dd47b236
Chase <crauhuff@csus.edu>
2022-11-19 08:26:10 -0800

    Added yes/no radio buttons and back/next buttons

--- main/src/Assessment.js
+++ main/src/Assessment.js
@@ -3,8 +3,11 @@ import React from 'react';
 const Assessment = () => {
     return (
         <div className="assessment">
-            <p>Is a list of authorized users mainted that states their identity and role?</p>
-            <p> Are requests to make system changes authorized?</p>
+            <p id = "question"></p>
+            <input id = "yes" type = "radio" name = "questionAnswer"/> Yes<br/>
+            <input id = "no"  type = "radio" name = "questionAnswer"/> No<br/>
+            <input id = "back" type = "button" value = "Back" onclick="displayPreviousQuestion();" style = {{ visibility: 'hidden' }}/>
+            <input type = "button" value = "Next" onclick="displayNextQuestion();"/> 
         </div>
     )
 }
