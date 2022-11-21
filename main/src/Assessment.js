b5b55dd039e6f84a7d80164416713d8280b109a4
Josiah <josiahvillarante@gmail.com>
2022-11-21 02:12:31 -0800

    Reformatted the results page to reflect the mockup. Added containers to store the results and the user's information.

--- main/src/Assessment.js
+++ main/src/Assessment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Assessment = () => {
     return (
         <div className="assessment">
-            <p>Is a list of authorized users mainted that states their identity and role?</p>
+            <p>Is a list of authorized users maintained that states their identity and role?</p>
             <p> Are requests to make system changes authorized?</p>
         </div>
     )
