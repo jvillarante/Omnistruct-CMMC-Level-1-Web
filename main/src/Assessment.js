9b7192b1bbd3ba08d6c7bab50d0cca2424c31b29
Chase <crauhuff@csus.edu>
2022-11-27 14:03:22 -0800

    Added more id attributes to elements to change colors and buttons

--- main/src/Assessment.js
+++ main/src/Assessment.js
@@ -42,13 +42,17 @@ const Assessment = () => {
     return (
         <div className="assessment">
             <p id = "question">Is a list of authorized users maintained that states their identity and role?</p>
-            <p>
-                <input id = "yes" type = "radio" name = "questionAnswer"/> Yes<br/>
-                <input id = "no"  type = "radio" name = "questionAnswer"/> No<br/>
+            <p className= "responses">
+                <input id = "yes" type = "radio" name = "questionAnswer"/> <label id = "yesLabel" for="yes"> Yes</label><br/>
+                <input id = "no"  type = "radio" name = "questionAnswer"/> <label id = "noLabel" for="no"> No</label><br/>
+            </p>
+
+            <p className= "navButtons">
                 <input id = "back" type = "button" value = "Back" onClick={displayPreviousQuestion} style = {{visibility: 'hidden' }}/>
-                <input type = "button" value = "Next" onClick={displayNextQuestion}/> 
+                <input id = "next" type = "button" value = "Next" onClick={displayNextQuestion}/> 
                 <Link to="/results">Results</Link>
             </p>
+
 
             <div className = "questionBank">
                 <h1>
@@ -57,19 +61,19 @@ const Assessment = () => {
                 <div className="questionButtons">
                     <nav>
                         < ul>
-                             <li><a href="#"><button>Question 1</button></a></li>
-                             <li><a href="#"><button>Question 2</button></a></li>
-                             <li><a href="#"><button>Question 3</button></a></li>
-                             <li><a href="#"><button>Quesiton 4</button></a></li>
-                             <li><a href="#"><button>Question 5</button></a></li>
-                             <li><a href="#"><button>Question 6</button></a></li> 
-                             <li><a href="#"><button>Question 7</button></a></li> 
-                             <li><a href="#"><button>Question 8</button></a></li>
-                             <li><a href="#"><button>Question 9</button></a></li>
-                             <li><a href="#"><button>Question 10</button></a></li>
-                             <li><a href="#"><button>Question 11</button></a></li>
-                             <li><a href="#"><button>Question 12</button></a></li>
-                             <li><a href="#"><button>Question 13</button></a></li>
+                             <li><a href="#"><button className= "H">Question 1</button></a></li>
+                             <li><a href="#"><button className= "H">Question 2</button></a></li>
+                             <li><a href="#"><button className= "H">Question 3</button></a></li>
+                             <li><a href="#"><button className= "H">Question 4</button></a></li>
+                             <li><a href="#"><button className= "H">Question 5</button></a></li>
+                             <li><a href="#"><button className= "H">Question 6</button></a></li> 
+                             <li><a href="#"><button className= "H">Question 7</button></a></li> 
+                             <li><a href="#"><button className= "H">Question 8</button></a></li>
+                             <li><a href="#"><button className= "H">Question 9</button></a></li>
+                             <li><a href="#"><button className= "H">Question 10</button></a></li>
+                             <li><a href="#"><button className= "H">Question 11</button></a></li>
+                             <li><a href="#"><button className= "H">Question 12</button></a></li>
+                             <li><a href="#"><button className= "H">Question 13</button></a></li>
                     </ul>
                 </nav>
                 </div>
